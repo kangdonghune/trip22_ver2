18bfabca4f791031ae330f6967fff62d244770b0
jinjin <oh9610@naver.com>
2020-06-19 22:34:25 +0900

    eva

--- app/src/main/java/com/sw/HeyBuddy2/EvaluationActivity.java
+++ app/src/main/java/com/sw/HeyBuddy2/EvaluationActivity.java
@@ -3,14 +3,24 @@ package com.sw.HeyBuddy2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,46 +34,154 @@ import xyz.hasnat.sweettoast.SweetToast;
 
 public class EvaluationActivity extends AppCompatActivity {
 
-    RatingBar point;
-    EditText uiuncom, help, manuuncom, price, etc ;
-    Button submit;
-    private FirebaseFirestore db;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_evaluation);
 
-        point = (RatingBar) findViewById(R.id.rating);
+        if(Build.VERSION.SDK_INT >= 21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        setContentView(R.layout.activity_tutorial);
 
-        uiuncom = (EditText) findViewById(R.id.uiuncom);
-        help = (EditText) findViewById(R.id.help);
-        manuuncom = (EditText) findViewById(R.id.manuuncom);
-        price = (EditText) findViewById(R.id.price);
-        etc = (EditText) findViewById(R.id.etc);
+        viewPager = findViewById(R.id.view_pager);
+        dotsLayout = findViewById(R.id.layoutDots);
+        btnSkip = findViewById(R.id.skip);
+        btnNext = findViewById(R.id.next);
 
-        submit = (Button) findViewById(R.id.btn_submit);
+        layouts = new int[]{
+                R.layout.eva001,
+                R.layout.eva002,
+                R.layout.eva003,
+                R.layout.eva004,
+                R.layout.eva005
+        };
 
-        db = FirebaseFirestore.getInstance();
-        submit.setOnClickListener(new View.OnClickListener() {
+        addBottomDots(0);
+        changeStatusBarColor();
+
+        pagerAdapter = new PagerAdapter();
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+
+        btnSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                evalUser();
-                Intent intent = new Intent(getApplication(), SelectActivity.class);
-                startActivity(intent);
-            }});
-    }
-    private void evalUser(){
-        DocumentReference evalDoc = db.collection("app_Evaluation").document();
-        String evalDocId = evalDoc.getId();
-        HashMap evalInfo = new HashMap();
-        evalInfo.put("rating", Float.toString(point.getRating()));
-        evalInfo.put("help section",help.getText().toString());
-        evalInfo.put("price limit", price.getText().toString());
-        evalInfo.put("UIuncomfortable", uiuncom.getText().toString());
-        evalInfo.put("manual uncomfortable", manuuncom.getText().toString());
-        evalInfo.put("etc", etc.getText().toString());
-        db.collection("app_Evaluation").document(evalDocId).set(evalInfo);
+                moveMainPage();
+            }
+        });
 
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = getItem(+1);
+                if(current < layouts.length){
+                    viewPager.setCurrentItem(current);
+                }else{
+                    moveMainPage();
+                }
+            }
+        });
+
+    }
+
+    private void addBottomDots(int currentPage){
+        dots = new TextView[layouts.length];
+
+
+        dotsLayout.removeAllViews();
+        for(int i = 0; i< dots.length; i++){
+            dots[i] = new TextView(this);
+            dots[i].setTextSize(35);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if(dots.length > 0);
+    }
+
+    private int getItem(int i){
+        return viewPager.getCurrentItem() + i;
+    }
+
+    private void moveMainPage(){
+        finish();
+    }
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            if(position == layouts.length - 1){
+                btnNext.setText(getString(R.string.start));
+                btnSkip.setVisibility(View.GONE);
+            }else{
+                btnNext.setText(getString(R.string.NEXT));
+                btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private void changeStatusBarColor(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+
+
+
+    public class PagerAdapter extends androidx.viewpager.widget.PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public PagerAdapter(){
+
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position){
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public int getCount(){
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view,Object obj){
+            return view == obj;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object){
+            View view = (View) object;
+            container.removeView(view);
+        }
     }
 }
