f18a145f149fbb8c5de683b4300697ae4ec54e06
KimGyeongHeon <kyasd658@naver.com>
2020-06-22 00:17:58 +0900

    신고

---
+++ app/src/main/java/com/sw/HeyBuddy2/FindBuddy/Report.java
@@ -0,0 +1,16 @@
+package com.sw.HeyBuddy2.FindBuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sw.HeyBuddy2.R;
+
+public class Report extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}
